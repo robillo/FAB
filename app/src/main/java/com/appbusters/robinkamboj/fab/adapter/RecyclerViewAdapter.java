3a620d95baa523034c1da54968fc0debdfba91f6
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-11-28 02:17:04 +0530

    Added RV Physically

--- app/src/main/java/com/appbusters/robinkamboj/fab/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/appbusters/robinkamboj/fab/adapter/RecyclerViewAdapter.java
@@ -4,5 +4,5 @@ package com.appbusters.robinkamboj.fab.adapter;
  * Created by robinkamboj on 28/11/16.
  */
 
-public class ListAdapter {
+public class RecyclerViewAdapter {
 }
--- app/src/main/java/com/appbusters/robinkamboj/fab/model/Data.java
+++ app/src/main/java/com/appbusters/robinkamboj/fab/model/Data.java
@@ -4,5 +4,5 @@ package com.appbusters.robinkamboj.fab.model;
  * Created by robinkamboj on 28/11/16.
  */
 
-public class ListItem {
+public class Data {
 }
