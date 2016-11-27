65f9a3aa41c0ee1feb9770a92571b1b4f1b371de
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-11-28 01:34:40 +0530

    created required respective classes to MVC

---
+++ app/src/main/java/com/appbusters/robinkamboj/fab/adapter/ListAdapter.java
@@ -0,0 +1,8 @@
+package com.appbusters.robinkamboj.fab.adapter;
+
+/**
+ * Created by robinkamboj on 28/11/16.
+ */
+
+public class ListAdapter {
+}
---
+++ app/src/main/java/com/appbusters/robinkamboj/fab/model/ListItem.java
@@ -0,0 +1,8 @@
+package com.appbusters.robinkamboj.fab.model;
+
+/**
+ * Created by robinkamboj on 28/11/16.
+ */
+
+public class ListItem {
+}
