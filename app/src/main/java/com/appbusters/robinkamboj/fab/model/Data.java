f87272dc9375f32812368f30dbcdda889436627a
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-11-28 02:31:31 +0530

    On It! RV preps almost done

--- app/src/main/java/com/appbusters/robinkamboj/fab/model/Data.java
+++ app/src/main/java/com/appbusters/robinkamboj/fab/model/Data.java
@@ -5,4 +5,13 @@ package com.appbusters.robinkamboj.fab.model;
  */
 
 public class Data {
+    public String title;
+    public String description;
+    public int imageId;
+
+    Data(String title,String description,int imageId){
+        this.title=title;
+        this.description=description;
+        this.imageId=imageId;
+    }
 }
--- app/src/main/java/com/appbusters/robinkamboj/fab/model/View_Holder.java
+++ app/src/main/java/com/appbusters/robinkamboj/fab/model/View_Holder.java
@@ -1,8 +1,29 @@
 package com.appbusters.robinkamboj.fab.model;
 
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.appbusters.robinkamboj.fab.R;
+
 /**
  * Created by robinkamboj on 28/11/16.
  */
 
-public class View_Holder {
+public class View_Holder extends RecyclerView.ViewHolder{
+
+    CardView cv;
+    TextView title;
+    TextView description;
+    ImageView imageView;
+
+    View_Holder(View viewItem){
+        super(viewItem);
+        cv= (CardView) viewItem.findViewById(R.id.cardView);
+        title= (TextView) viewItem.findViewById(R.id.title);
+        description= (TextView) viewItem.findViewById(R.id.description);
+        imageView= (ImageView) viewItem.findViewById(R.id.imageView);
+    }
 }
