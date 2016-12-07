946d9ffb6ff14a1ead6cd0ca79328525a6f7816f
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-07 19:43:59 +0530

    Recyclerview onClick to another Activity

--- app/src/main/java/com/appbusters/robinkamboj/fab/model/View_Holder.java
+++ app/src/main/java/com/appbusters/robinkamboj/fab/model/View_Holder.java
@@ -1,5 +1,7 @@
 package com.appbusters.robinkamboj.fab.model;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -7,14 +9,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.appbusters.robinkamboj.fab.R;
+import com.appbusters.robinkamboj.fab.ui.Level1_extended;
 
 /**
  * Created by robinkamboj on 28/11/16.
  */
 
-public class View_Holder extends RecyclerView.ViewHolder{
+public class View_Holder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     CardView cv;
+    Context context;
     public TextView title;
     public TextView description;
     public ImageView imageView;
@@ -25,5 +29,11 @@ public class View_Holder extends RecyclerView.ViewHolder{
         title= (TextView) viewItem.findViewById(R.id.title);
         description= (TextView) viewItem.findViewById(R.id.description);
         imageView= (ImageView) viewItem.findViewById(R.id.imageView);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i= new Intent(context, Level1_extended.class);
+        context.startActivity(i);
     }
 }
---
+++ app/src/main/java/com/appbusters/robinkamboj/fab/ui/Level1_extended.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinkamboj.fab.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.fab.R;
+
+public class Level1_extended extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level1_extended);
+    }
+}
